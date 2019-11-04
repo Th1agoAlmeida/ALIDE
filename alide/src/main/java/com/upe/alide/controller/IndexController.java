f0abbce9f48e5b0669a49d34206aed22b198af30
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2019-11-04 11:17:14 -0300

    Modificando arquivos pom.xml IndexController.java Index.html

--- alide/src/main/java/com/upe/alide/controller/IndexController.java
+++ alide/src/main/java/com/upe/alide/controller/IndexController.java
@@ -1,18 +1,11 @@
 package com.upe.alide.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.upe.alide.model.Aluno;
-import com.upe.alide.repository.IndexRepository;
-
 @Controller
 public class IndexController {
-	
-	@Autowired
-	private IndexRepository alunoEntity;
 	
 	@RequestMapping(value = {"/", "index.html", "home"}, method = RequestMethod.GET)
 	public String index() {
