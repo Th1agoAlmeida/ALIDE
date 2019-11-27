a27ca8538231a23a61b64ae5e6368802c0f46b42
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2019-11-27 16:18:37 -0300

    add arquivos do quiz a plataforma

---
+++ alide/src/main/resources/static/js/questions.js
@@ -0,0 +1,50 @@
+var questions = [{
+    "question": "Quais são os tipos primitivos na linguagem de programação java?",
+    "option1": "boolean, byte, short, long, float, double, int, char",
+    "option2": "boolean, short, long, float, double, int, char",
+    "option3": "boolean, byte, short, long, float, double, int",
+    "option4": "boolean, float, double, int",
+    "answer": "3"
+}, {
+    "question": "Em uma expressão que tem mais de 1 operador aritmético é necessário fazer uma análise de quais operações devem ser  resolvidas primeiro. Qual a ordem de precedência dos operadores aritméticos?",
+    "option1": "primeiro multiplicação; segundo divisão; terceiro resto (%); quarto soma; quinto subtração",
+    "option2": "primeiro divisão; segundo multiplicação; terceiro resto (%); quarto soma; quinto subtração",
+    "option3": "primeiro multiplicação e divisão; segundo resto (%); terceiro soma; quarto subtração",
+    "option4": "primeiro multiplicação, divisão e resto (%), resolvendo quem aparecer primeiro;  segundo soma  e subtração também resolvendo quem aparecer primeiro",
+    "answer": "4"
+}, {
+    "question": " O valor da declaração a seguir gera qual resultado: String var = “10” + 1",
+    "option1": "10+1",
+    "option2": "101",
+    "option3": "“11”",
+    "option4": "11",
+    "answer": "‘101’"
+}, {
+    "question": "Analise as seguintes frases e assinale a alternativa correta: I. Conjunto de programas. II. Usuários ou profissionais em TI. III. Parte física do computador.",
+    "option1": "I- Hardware, II- Software, III- Firmware.",
+    "option2": "I- Software, II- Firmware, III- Hardware.",
+    "option3": "Software, II- Peopleware, III- Hardware.",
+    "option4": "I - Software, II- Personware, III- Hardware.",
+    "answer": "3    "
+}, {
+    "question": "HTML é uma linguagem de:",
+    "option1": "Programação.",
+    "option2": "Formatação.",
+    "option3": "Visualização.",
+    "option4": "Programação de web.",
+    "answer": "2"
+}, {
+    "question": "Qual das seguintes variáveis só aceita números inteiros?",
+    "option1": "Single.",
+    "option2": "String.",
+    "option3": "Interger.",
+    "option4": "Double.",
+    "answer": "3"
+}, {
+    "question": "Assinale a alternativa que não contém uma regra para nomeação de variáveis.",
+    "option1": "Não pode ser palavra reservada.",
+    "option2": "Não podem ter números.",
+    "option3": "Não pode haver espaço.",
+    "option4": "Devem começar sempre com letras.",
+    "answer": "3"
+}];
