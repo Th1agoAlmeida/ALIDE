25e8024d396ac426600ead7a82e6bd6ac02352f1
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2019-11-28 17:27:29 -0300

    add modificacoes nao afetadoras ao cósigo

--- alide/src/main/resources/static/js/quiz-script.js
+++ alide/src/main/resources/static/js/quiz-script.js
@@ -2,6 +2,7 @@
 var currentQuestion = 0;
 var score = 0;
 var totQuestions = questions.length;
+var respostasErradas = [];
 
 var container = document.getElementById('quizContainer');
 var question = document.getElementById('question');
@@ -12,37 +13,59 @@ var opt4 = document.getElementById('opt4');
 var nextButton = document.getElementById('nextButton');
 var resultCont = document.getElementById('result');
 
-function loadQuestion (questionIndex) {
-	var q = questions[questionIndex];
-	question.textContent = (questionIndex + 1) + '. ' + q.question;
+function resultadoFinal() {
+	
+	container.style.display = 'none';
+	resultCont.style.display = '';
+
+	// resultCont.textContent = 'Sua pontuação: ' + score;
+
+	let paragrafo_resutado = document.createElement('p');
+	paragrafo_resutado.innerHTML = `Resultado: ${score} Pontos`;
+
+
+	resultCont.appendChild(paragrafo_resutado);
+	// let paragrafo_recomendacoes = 
+} 
+
+function loadQuestion(index) {
+	let q = questions[index];
+	question.textContent = (index + 1) + '. ' + q.question;
 	opt1.textContent = q.option1;
 	opt2.textContent = q.option2;
 	opt3.textContent = q.option3;
 	opt4.textContent = q.option4;
 };
 
-function loadNextQuestion () {
-	var selectedOption = document.querySelector('input[type=radio]:checked');
+function loadNextQuestion() {
+
+	let selectedOption = document.querySelector('input[type=radio]:checked');
+
 	if(!selectedOption){
+
 		alert('Por favor, selecione uma alternativa!');
-		return;
+
 	}
+
 	var answer = selectedOption.value;
-	if(questions[currentQuestion].answer == answer){
+
+	if(questions[currentQuestion].answer == answer){ // Se questão correta score recebe +10 pt senão numero da questão guarde para recber recomendação de estudo
 		score += 10;
+	} else {
+		respostasErradas.push(currentQuestion);
 	}
-	selectedOption.checked = false;
+	
 	currentQuestion++;
-	if(currentQuestion == totQuestions - 1){
+
+	if(currentQuestion == totQuestions - 1){ // Verifica até ultima questão, quando chegar a ultima questão troca o nome do botão
 		nextButton.textContent = 'Finish';
 	}
+
 	if(currentQuestion == totQuestions){
-		container.style.display = 'none';
-		resultCont.style.display = '';
-		resultCont.textContent = 'Sua pontuação: ' + score;
-		return;
+		resultadoFinal();
 	}
+
 	loadQuestion(currentQuestion);
 }
 
-loadQuestion(currentQuestion);
+loadQuestion(currentQuestion); // Load Inicial
